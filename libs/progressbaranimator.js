32fc4c4b3478dff44e1317da471bef72bd076eba
Matei Suica <matei.suica@gmail.com>
2017-02-02 21:30:34 +0200

    more work on progressBars

--- libs/progressbaranimator.js
+++ libs/progressbaranimator.js
@@ -1,8 +1,28 @@
 
 window.onload = function onLoad() {
-var progressBar = new ProgressBar.Line('#android-skills', {
+
+var androidBar = new ProgressBar.Line('#android-skills', {
     strokeWidth: 2,
      color: '#FCB03C'
 });
-progressBar.animate(0.78, {duration: 5000, easing: 'easeInOut'});
+androidBar.animate(0.98, {duration: 2000, easing: 'easeInOut'});
+
+var iosBar = new ProgressBar.Line('#ios-skills', {
+    strokeWidth: 2,
+     color: '#FCB03C'
+});
+iosBar.animate(0.78, {duration: 2000, easing: 'easeInOut'});
+
+var angularBar = new ProgressBar.Line('#angular-skills', {
+    strokeWidth: 2,
+     color: '#FCB03C'
+});
+angularBar.animate(0.50, {duration: 2000, easing: 'easeInOut'});
+
+var rorBar = new ProgressBar.Line('#ror-skills', {
+    strokeWidth: 2,
+     color: '#FCB03C'
+});
+rorBar.animate(0.50, {duration: 2000, easing: 'easeInOut'});
+
 };
