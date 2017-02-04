65aa29b990e175b395f6013503ada9f0555906a8
Matei Suica <matei.suica@gmail.com>
2017-02-04 19:25:42 +0200

    Contact form works

--- contact.php
+++ contact.php
@@ -1,40 +1,39 @@
 <?php
-
+ require_once "Mail.php";
 // configure
 $from = 'Matei Suica <contact@mateisuica.com>';
-$sendTo = 'Matei Suica <contact@mateisuica.com>';
+$to = 'Matei Suica <matei.suica@gmail.com>';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
+$host = "mail.mateisuica.com";
+$username = "contact@mateisuica.com";
+$password = "8T.1bV$-bOfn";
 // let's do the sending
 
-try
-{
-    $emailText = "You have new message from contact form\n=============================\n";
+    $body = "You have new message from contact form\n=============================\n";
 
     foreach ($_POST as $key => $value) {
 
         if (isset($fields[$key])) {
-            $emailText .= "$fields[$key]: $value\n";
+            $body .= "$fields[$key]: $value\n";
         }
     }
 
-    $headers = array('Content-Type: text/html; charset="UTF-8";',
-        'From: ' . $from,
-        'Reply-To: ' . $from,
-        'Return-Path: ' . $from,
-    );
+    $headers = array ('From' => $from,
+       'To' => $to,
+       'Subject' => $subject);
 
-    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+    $smtp = Mail::factory('smtp',
+      array ('host' => $host,
+        'auth' => true,
+        'username' => $username,
+        'password' => $password));
 
+    $mail = $smtp->send($to, $headers, $body);
     $responseArray = array('type' => 'success', 'message' => $okMessage);
-}
-catch (\Exception $e)
-{
-    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-}
 
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
     $encoded = json_encode($responseArray);
